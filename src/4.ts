faf869d8be827e5edb537e72a311ddd8bd3af532
Viktoria <vik25@i.ua>
2023-09-19 12:39:25 +0200

    add homework-3

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,55 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey() {
+    return this.key.getSignature();
+  }
+}
+
+abstract class House {
+  constructor(public door: boolean, protected key: Key) {}
+  tenants: Person[] = [];
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(person: Person): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(false, key);
+  }
+
+  openDoor(person: Person) {
+    if (person.getKey() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Двері відчинені");
+    } else {
+      console.log("Двері зачинені");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.openDoor(person);
 
 house.comeIn(person);
-
 
 export {};
